3ba2e259988742dc4dde97ca3b4b91bf5b4bb06d
Neha <nehanegi1507@gmail.com>
2022-12-22 11:48:30 -0500

    --changes to queries

--- Queries.sql
+++ Queries.sql
@@ -1,97 +1,114 @@
---Requirement 1: Top 2 Categories
---For each customer, we need to identify the top 2 categories for each customer based off their past rental history. 
---These top categories will drive marketing creative images as seen in the travel and sci-fi examples in the draft email.
-
-
---First created a complete_joint_dataset which joins multiple tables together after analysing the relationships between 
---each table to confirm if there was a one-to-many, many-to-one or a many-to-many relationship for each of the join columns.
---Also included the rental_date column to help us split ties for rankings which had the same count of rentals at a customer 
---level - this helps us prioritise film categories which were more recently viewed.
-
-DROP TABLE IF EXISTS complete_joint_dataset;
-CREATE TEMP TABLE complete_joint_dataset AS
-SELECT
-  rental.customer_id,
-  inventory.film_id,
-  film.title,
-  rental.rental_date,
-  category.name AS category_name
-FROM dvd_rentals.rental
-INNER JOIN dvd_rentals.inventory
-  ON rental.inventory_id = inventory.inventory_id
-INNER JOIN dvd_rentals.film
-  ON inventory.film_id = film.film_id
-INNER JOIN dvd_rentals.film_category
-  ON film.film_id = film_category.film_id
-INNER JOIN dvd_rentals.category
-  ON film_category.category_id = category.category_id;
-
-SELECT * FROM complete_joint_dataset limit 10;
-
---Creating Category Counts
---Created a follow-up table which uses the complete_joint_dataset to aggregate  data and 
---generate a rental_count and the latest rental_date for our ranking purposes downstream.
-
-DROP TABLE IF EXISTS category_counts;
-CREATE TEMP TABLE category_counts AS
-SELECT
-  customer_id,
-  category_name,
-  COUNT(*) AS rental_count,
-  MAX(rental_date) AS latest_rental_date
-FROM complete_joint_dataset
-GROUP BY
-  customer_id,
-  category_name;
-  
-SELECT *
-FROM category_counts
---WHERE customer_id = 1
-ORDER BY
-    customer_id,
-    rental_count DESC,
-    latest_rental_date DESC;
-    
-
--- Creating Top 2 Categories using a DENSE_RANK window function.
---To split ties ,recent lastest_rental_date value generated in category_counts. To further prevent any ties - 
---sort by the category_name in alphabetical (ascending) order.
-
-
-DROP TABLE IF EXISTS top_categories;
-CREATE TEMP TABLE top_categories AS
-WITH ranked_cte AS (
+DROP TABLE IF EXISTS final_data_asset;
+CREATE TEMP TABLE final_data_asset AS
+WITH first_category AS (
   SELECT
     customer_id,
     category_name,
-    rental_count,
-    DENSE_RANK() OVER (
-      PARTITION BY customer_id
-      ORDER BY
-        rental_count DESC,
-        latest_rental_date DESC,
-        category_name
-    ) AS category_rank
-  FROM category_counts
+    CONCAT(
+      'You''ve watched ', rental_count, ' ', category_name,
+      ' films, that''s ', average_comparison,
+      ' more than the DVD Rental Co average and puts you in the top ',
+      percentile, '% of ', category_name, ' gurus!'
+    ) AS insight
+  FROM first_category_insights
+),
+second_category AS (
+  SELECT
+    customer_id,
+    category_name,
+    CONCAT(
+      'You''ve watched ', rental_count, ' ', category_name,
+      ' films making up ', total_percentage,
+      '% of your entire viewing history!'
+    ) AS insight
+  FROM second_category_insights
+),
+top_actor AS (
+  SELECT
+    customer_id,
+    -- use INITCAP to transform names into Title case
+    CONCAT(INITCAP(first_name), ' ', INITCAP(last_name)) AS actor_name,
+    CONCAT(
+      'You''ve watched ', rental_count, ' films featuring ',
+      INITCAP(first_name), ' ', INITCAP(last_name),
+      '! Here are some other films ', INITCAP(first_name),
+      ' stars in that might interest you!'
+    ) AS insight
+  FROM top_actor_counts
+),
+adjusted_title_case_category_recommendations AS (
+  SELECT
+    customer_id,
+    INITCAP(title) AS title,
+    category_rank,
+    reco_rank
+  FROM category_recommendations
+),
+wide_category_recommendations AS (
+  SELECT
+    customer_id,
+    MAX(CASE WHEN category_rank = 1  AND reco_rank = 1
+      THEN title END) AS cat_1_reco_1,
+    MAX(CASE WHEN category_rank = 1  AND reco_rank = 2
+      THEN title END) AS cat_1_reco_2,
+    MAX(CASE WHEN category_rank = 1  AND reco_rank = 3
+      THEN title END) AS cat_1_reco_3,
+    MAX(CASE WHEN category_rank = 2  AND reco_rank = 1
+      THEN title END) AS cat_2_reco_1,
+    MAX(CASE WHEN category_rank = 2  AND reco_rank = 2
+      THEN title END) AS cat_2_reco_2,
+    MAX(CASE WHEN category_rank = 2  AND reco_rank = 3
+      THEN title END) AS cat_2_reco_3
+  FROM adjusted_title_case_category_recommendations
+  GROUP BY customer_id
+),
+adjusted_title_case_actor_recommendations AS (
+  SELECT
+    customer_id,
+    INITCAP(title) AS title,
+    reco_rank
+  FROM actor_recommendations
+),
+wide_actor_recommendations AS (
+  SELECT
+    customer_id,
+    MAX(CASE WHEN reco_rank = 1 THEN title END) AS actor_reco_1,
+    MAX(CASE WHEN reco_rank = 2 THEN title END) AS actor_reco_2,
+    MAX(CASE WHEN reco_rank = 3 THEN title END) AS actor_reco_3
+  FROM adjusted_title_case_actor_recommendations
+  GROUP BY customer_id
+),
+final_output AS (
+  SELECT
+    t1.customer_id,
+    t1.category_name AS cat_1,
+    t4.cat_1_reco_1,
+    t4.cat_1_reco_2,
+    t4.cat_1_reco_3,
+    t2.category_name AS cat_2,
+    t4.cat_2_reco_1,
+    t4.cat_2_reco_2,
+    t4.cat_2_reco_3,
+    t3.actor_name AS actor,
+    t5.actor_reco_1,
+    t5.actor_reco_2,
+    t5.actor_reco_3,
+    t1.insight AS insight_cat_1,
+    t2.insight AS insight_cat_2,
+    t3.insight AS insight_actor
+FROM first_category AS t1
+INNER JOIN second_category AS t2
+  ON t1.customer_id = t2.customer_id
+INNER JOIN top_actor t3
+  ON t1.customer_id = t3.customer_id
+INNER JOIN wide_category_recommendations AS t4
+  ON t1.customer_id = t4.customer_id
+INNER JOIN wide_actor_recommendations AS t5
+  ON t1.customer_id = t5.customer_id
 )
-SELECT * FROM ranked_cte
-WHERE category_rank <= 2;
-
-SELECT *
-FROM top_categories;
+SELECT * FROM final_output;
 
 
---Creating Total Counts table:category_counts table to generate total_counts table.
-
-DROP TABLE IF EXISTS total_counts;
-CREATE TEMP TABLE total_counts AS
-SELECT
-  customer_id,
-  SUM(rental_count) AS total_count
-FROM category_counts
-GROUP BY
-  customer_id;
-
-SELECT *
-FROM total_counts
+SELECT * FROM final_data_asset
 LIMIT 5;
+
