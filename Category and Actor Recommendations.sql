932c007c4c28c5e741463a06b4c71657680f9685
Neha <nehanegi1507@gmail.com>
2022-12-22 11:55:42 -0500

    --querries

---
+++ Category and Actor Recommendations.sql
@@ -0,0 +1,247 @@
+--1. Film Counts
+
+DROP TABLE IF EXISTS film_counts;
+CREATE TEMP TABLE film_counts AS
+SELECT DISTINCT
+  film_id,
+  title,
+  category_name,
+  COUNT(*) OVER (
+    PARTITION BY film_id
+  ) AS rental_count
+FROM complete_joint_dataset;
+
+SELECT *
+FROM film_counts
+ORDER BY rental_count DESC
+LIMIT 10;
+
+--2. Category Film Exclusions
+
+DROP TABLE IF EXISTS category_film_exclusions;
+CREATE TEMP TABLE category_film_exclusions AS
+SELECT DISTINCT
+  customer_id,
+  film_id
+FROM complete_joint_dataset;
+
+SELECT *
+FROM category_film_exclusions
+LIMIT 10;
+
+--3. Final Category Suggestions
+
+DROP TABLE IF EXISTS category_recommendations;
+CREATE TEMP TABLE category_recommendations AS
+WITH ranked_films_cte AS (
+  SELECT
+    top_categories.customer_id,
+    top_categories.category_name,
+    top_categories.category_rank,
+    film_counts.film_id,
+    film_counts.title,
+    film_counts.rental_count,
+    DENSE_RANK() OVER (
+      PARTITION BY
+        top_categories.customer_id,
+        top_categories.category_rank
+      ORDER BY
+        film_counts.rental_count DESC,
+        film_counts.title
+    ) AS reco_rank
+  FROM top_categories
+  INNER JOIN film_counts
+    ON top_categories.category_name = film_counts.category_name
+  -- This is a  anti-join where we need to "join" on 2 different tables!
+  WHERE NOT EXISTS (
+    SELECT 1
+    FROM category_film_exclusions
+    WHERE
+      category_film_exclusions.customer_id = top_categories.customer_id AND
+      category_film_exclusions.film_id = film_counts.film_id
+  )
+)
+SELECT * FROM ranked_films_cte
+WHERE reco_rank <= 3;
+
+SELECT *
+FROM category_recommendations
+WHERE customer_id = 1
+ORDER BY category_rank, reco_rank;
+
+-- Actor Insights
+-- 1. Actor Joint Table
+
+DROP TABLE IF EXISTS actor_joint_dataset;
+CREATE TEMP TABLE actor_joint_dataset AS
+SELECT
+  rental.customer_id,
+  rental.rental_id,
+  rental.rental_date,
+  film.film_id,
+  film.title,
+  actor.actor_id,
+  actor.first_name,
+  actor.last_name
+FROM dvd_rentals.rental
+INNER JOIN dvd_rentals.inventory
+  ON rental.inventory_id = inventory.inventory_id
+INNER JOIN dvd_rentals.film
+  ON inventory.film_id = film.film_id
+INNER JOIN dvd_rentals.film_actor
+  ON film.film_id = film_actor.film_id
+INNER JOIN dvd_rentals.actor
+  ON film_actor.actor_id = actor.actor_id;
+  
+SELECT
+  COUNT(*) AS total_row_count,
+  COUNT(DISTINCT rental_id) AS unique_rental_id,
+  COUNT(DISTINCT film_id) AS unique_film_id,
+  COUNT(DISTINCT actor_id) AS unique_actor_id,
+  COUNT(DISTINCT customer_id) AS unique_customer_id
+FROM actor_joint_dataset;
+
+-- show the first 10 rows
+SELECT *
+FROM actor_joint_dataset
+LIMIT 10;
+
+--2. Top Actor Counts
+DROP TABLE IF EXISTS top_actor_counts;
+CREATE TEMP TABLE top_actor_counts AS
+WITH actor_counts AS (
+  SELECT
+    customer_id,
+    actor_id,
+    first_name,
+    last_name,
+    COUNT(*) AS rental_count,
+    -- we also generate the latest_rental_date just like our category insight
+    MAX(rental_date) AS latest_rental_date
+  FROM actor_joint_dataset
+  GROUP BY
+    customer_id,
+    actor_id,
+    first_name,
+    last_name
+),
+ranked_actor_counts AS (
+  SELECT
+    actor_counts.*,
+    DENSE_RANK() OVER (
+      PARTITION BY customer_id
+      ORDER BY
+        rental_count DESC,
+        latest_rental_date DESC,
+        -- just in case we have any further ties, we'll add the names too!
+        first_name,
+        last_name
+    ) AS actor_rank
+  FROM actor_counts
+)
+SELECT
+  customer_id,
+  actor_id,
+  first_name,
+  last_name,
+  rental_count
+FROM ranked_actor_counts
+WHERE actor_rank = 1;
+
+SELECT *
+FROM top_actor_counts
+LIMIT 10;
+
+--Actor Recommendations
+
+--1.Actor Film Counts
+
+DROP TABLE IF EXISTS actor_film_counts;
+CREATE TEMP TABLE actor_film_counts AS
+WITH film_counts AS (
+  SELECT
+    film_id,
+    COUNT(DISTINCT rental_id) AS rental_count
+  FROM actor_joint_dataset
+  GROUP BY film_id
+)
+SELECT DISTINCT
+  actor_joint_dataset.film_id,
+  actor_joint_dataset.actor_id,
+  -- why do we keep the title here? can you figure out why?
+  actor_joint_dataset.title,
+  film_counts.rental_count
+FROM actor_joint_dataset
+LEFT JOIN film_counts
+  ON actor_joint_dataset.film_id = film_counts.film_id;
+  
+SELECT *
+FROM actor_film_counts
+LIMIT 10;
+
+--2. Film Actor Exclusions
+
+DROP TABLE IF EXISTS actor_film_exclusions;
+CREATE TEMP TABLE actor_film_exclusions AS
+-- repeat the first steps as per the category exclusions
+-- we'll use our original complete_joint_dataset as the base here
+(
+  SELECT DISTINCT
+    customer_id,
+    film_id
+  FROM complete_joint_dataset
+)
+-- we use a UNION to combine the previously watched and the recommended films
+UNION
+(
+  SELECT DISTINCT
+    customer_id,
+    film_id
+  FROM category_recommendations
+);
+
+SELECT *
+FROM actor_film_exclusions
+LIMIT 10;
+
+--3. Final Actor eEcommendations
+
+DROP TABLE IF EXISTS actor_recommendations;
+CREATE TEMP TABLE actor_recommendations AS
+WITH ranked_actor_films_cte AS (
+  SELECT
+    top_actor_counts.customer_id,
+    top_actor_counts.first_name,
+    top_actor_counts.last_name,
+    top_actor_counts.rental_count,
+    actor_film_counts.title,
+    actor_film_counts.film_id,
+    actor_film_counts.actor_id,
+    DENSE_RANK() OVER (
+      PARTITION BY
+        top_actor_counts.customer_id
+      ORDER BY
+        actor_film_counts.rental_count DESC,
+        actor_film_counts.title
+    ) AS reco_rank
+  FROM top_actor_counts
+  INNER JOIN actor_film_counts
+    -- join on actor_id instead of category_name!
+    ON top_actor_counts.actor_id = actor_film_counts.actor_id
+  -- This is a tricky anti-join where we need to "join" on 2 different tables!
+  WHERE NOT EXISTS (
+    SELECT 1
+    FROM actor_film_exclusions
+    WHERE
+      actor_film_exclusions.customer_id = top_actor_counts.customer_id AND
+      actor_film_exclusions.film_id = actor_film_counts.film_id
+  )
+)
+SELECT * FROM ranked_actor_films_cte
+WHERE reco_rank <= 3;
+
+
+SELECT *
+FROM actor_recommendations
+ORDER BY customer_id, reco_rank
+LIMIT 15;
